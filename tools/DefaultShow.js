a08f4541c5a21ac44da2dd99dc9714c50ded946e
xander <26496749+cshsonice@users.noreply.github.com>
2018-11-28 17:26:35 +0800

    默认展示内容组件

---
+++ tools/DefaultShow.js
@@ -0,0 +1,11 @@
+/*jshint esversion: 6 */
+
+import React, { Component } from "react";
+
+export default class DefaultShow extends React.Component{
+    render(){
+        return (
+            <h2>welcome to use the tools</h2>
+        );
+    }
+}
