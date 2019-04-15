92b1297cfa2124957ba6a533a97be8ec0c595ba1
xander <26496749+cshsonice@users.noreply.github.com>
2019-04-15 17:46:01 +0800

    调整侧边栏显示项

--- MySider.js
+++ MySider.js
@@ -44,21 +44,15 @@ class MySider extends React.Component{
           >
             <SubMenu key="ctf" title={<span><Icon type="tool" />ctf</span>}>
               <Menu.Item key="1">移位变换</Menu.Item>
-              <Menu.Item key="2">option2</Menu.Item>
-              <Menu.Item key="3">option3</Menu.Item>
-              <Menu.Item key="4">option4</Menu.Item>
             </SubMenu>
             <SubMenu key="funny" title={<span><Icon type="bulb" />funny</span>}>
               <Menu.Item key="5">粘贴板</Menu.Item>
-              <Menu.Item key="6">option6</Menu.Item>
-              <Menu.Item key="7">option7</Menu.Item>
-              <Menu.Item key="8">option8</Menu.Item>
             </SubMenu>
             <SubMenu key="others" title={<span><Icon type="rocket" />others</span>}>
-              <Menu.Item key="9">option9</Menu.Item>
-              <Menu.Item key="10">option10</Menu.Item>
-              <Menu.Item key="11">option11</Menu.Item>
-              <Menu.Item key="12">option12</Menu.Item>
+              <Menu.Item key="9">option1</Menu.Item>
+              <Menu.Item key="10">option2</Menu.Item>
+              <Menu.Item key="11">option3</Menu.Item>
+              <Menu.Item key="12">option4</Menu.Item>
             </SubMenu>
           </Menu>
         </Sider>
