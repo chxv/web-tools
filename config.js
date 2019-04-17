4239d226da99c7892d6a24afc59e623dca7ec784
xander <26496749+cshsonice@users.noreply.github.com>
2019-04-17 18:26:50 +0800

    update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 
 export const configs = {
-    'api-host': 'ali.xchens.cn:8008',
+    'api-host': 'api.xchens.cn:8443',
     // 'api-host': '127.0.0.1',
-    'api-method': 'http'
+    'api-method': 'https'
 };
